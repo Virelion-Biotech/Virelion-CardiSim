042ea666215d31d132ad056bb5e9c744b3357c3d
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-09-14 17:38:26 +0300

    style: normalize Cardisim public API exports

--- src/cardisim/__init__.py
+++ src/cardisim/__init__.py
@@ -1,34 +1,73 @@
 """Public API for Virelion-CardiSim."""
 
 from .atlas_bridge import calibrate_atlas_export, validate_atlas_manifest
-from .calibration import CalibrationResult, EmpiricalTrajectory, calibrate, calibration_from_csv, load_long_csv
+from .calibration import (
+    CalibrationResult,
+    EmpiricalTrajectory,
+    calibrate,
+    calibration_from_csv,
+    load_long_csv,
+)
 from .cohort_calibration import CohortCalibrationResult, CohortObservation, calibrate_cohort
 from .deepcardiosim import DEEPCARDIOSIM_REFERENCE, ReferenceRecord, deepcardiosim_reference
 from .deepcardiosim_data import DeepCardioSimSample
 from .dynamics import DynamicsParameters
 from .events import ChallengeEvent, EventSchedule
-from .geo_sources import SOURCES, GeoSource, download
 from .geo10x import sparse_module_scores
+from .geo_sources import SOURCES, GeoSource, download
 from .geometry_reference import EPPreprocessor, UnitGaussianNormalizer
-from .longitudinal import LongitudinalCalibrationReport, LongitudinalCalibrationResult, Observation, calibrate_longitudinal
+from .longitudinal import (
+    LongitudinalCalibrationReport,
+    LongitudinalCalibrationResult,
+    Observation,
+    calibrate_longitudinal,
+)
 from .marker_modules import PROXY_MODULES
 from .models import PHENOTYPES, CardiacState, SimulationConfig
 from .presets import population_preset
-from .proxy_targets import module_scores, mean_target
+from .proxy_targets import mean_target, module_scores
 from .simulate import CardiacSimulator, SimulationResult
 from .target_derivation import derive_targets, write_long_targets
 
 __all__ = [
-    "CardiacSimulator", "SimulationConfig", "SimulationResult", "CardiacState",
-    "ChallengeEvent", "EventSchedule", "PHENOTYPES", "population_preset",
-    "DynamicsParameters", "EmpiricalTrajectory", "CalibrationResult", "calibrate",
-    "load_long_csv", "calibration_from_csv", "validate_atlas_manifest", "calibrate_atlas_export",
-    "GeoSource", "SOURCES", "download", "sparse_module_scores", "PROXY_MODULES",
-    "derive_targets", "write_long_targets", "Observation", "LongitudinalCalibrationReport",
-    "LongitudinalCalibrationResult", "calibrate_longitudinal", "CohortObservation",
-    "CohortCalibrationResult", "calibrate_cohort", "module_scores", "mean_target",
-    "ReferenceRecord", "DEEPCARDIOSIM_REFERENCE", "deepcardiosim_reference",
-    "DeepCardioSimSample", "EPPreprocessor", "UnitGaussianNormalizer",
+    "DEEPCARDIOSIM_REFERENCE",
+    "PHENOTYPES",
+    "PROXY_MODULES",
+    "SOURCES",
+    "CalibrationResult",
+    "CardiacSimulator",
+    "CardiacState",
+    "ChallengeEvent",
+    "CohortCalibrationResult",
+    "CohortObservation",
+    "DeepCardioSimSample",
+    "DynamicsParameters",
+    "EPPreprocessor",
+    "EmpiricalTrajectory",
+    "EventSchedule",
+    "GeoSource",
+    "LongitudinalCalibrationReport",
+    "LongitudinalCalibrationResult",
+    "Observation",
+    "ReferenceRecord",
+    "SimulationConfig",
+    "SimulationResult",
+    "UnitGaussianNormalizer",
+    "calibrate",
+    "calibrate_atlas_export",
+    "calibrate_cohort",
+    "calibrate_longitudinal",
+    "calibration_from_csv",
+    "deepcardiosim_reference",
+    "derive_targets",
+    "download",
+    "load_long_csv",
+    "mean_target",
+    "module_scores",
+    "population_preset",
+    "sparse_module_scores",
+    "validate_atlas_manifest",
+    "write_long_targets",
 ]
 
 __version__ = "0.3.0"
